ea1e36ec5be986aafbb6427614d1a4b9cb2c0ae8
aaroncbader <abader@engr.wisc.edu>
2022-08-17 13:41:23 -0500

    add export commands

--- src/StellaratorGrids.jl
+++ src/StellaratorGrids.jl
@@ -9,6 +9,10 @@ using PlasmaEquilibriumToolkit
 
 export expanded_wall_auto, expanded_wall_simple
 
+#flare utils
+export read_flare_strike, get_flare_strike_2d, read_flare_wall
+
 include("expand_surface.jl")
+include("FlareUtils.jl")
 
 end
